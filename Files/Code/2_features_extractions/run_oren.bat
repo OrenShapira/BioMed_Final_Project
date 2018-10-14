1c672c5e611cf8a8234b5f11a71c228f449be244
oren shapira <orenshapi@gmail.com>
2018-10-14 09:35:58 +0300

    version 2 - Finish 2_features_extractions
    * Finish 2_features_extractions

---
+++ Files/Code/2_features_extractions/run_oren.bat
@@ -0,0 +1,22 @@
+:: print some notes
+@echo off
+echo User:   Oren
+echo Script: feature_extraction (jupyter notebook)
+echo Note1:  if failed - run as administrator or check paths!
+echo ...
+
+:: 1. call anaconda prompt
+echo 1. call anaconda prompt
+call "D:\Softwares\Anaconda3\Scripts\activate.bat"
+
+:: 2. set project directory (according to computer OS)
+echo 2. set project directory
+for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
+if "%version%" == "10.0" (
+cd "C:\Users\orens\Documents\GitHub\BioMed_Final_Project\Files\Code\2_features_extractions"
+) else (
+cd "C:\Users\אורן\Documents\GitHub\BioMed_Final_Project\Files\Code\2_features_extractions")
+
+:: 3. start jupyter notebook
+echo 3. start jupyter notebook
+start jupyter notebook
